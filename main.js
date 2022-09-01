431a1bf9117a3ea240edc3e8e877f079b9c94ca6
Hadi Azeem <hadiazeem.azeemi@gmail.com>
2022-09-01 23:14:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const startBtn = document.getElementById("Start-btn");
+const elem = document.getElementById("myBar");
+const bgImg = document.getElementById("Bg-image");
+
+bgImg.classList.add("blur");
+
+let root_speed = getComputedStyle(document.documentElement);
+
+console.log(root_speed.getPropertyValue("--speed"));
+
+document.documentElement.style.setProperty("--speed", "15s");
+
+console.log(root_speed.getPropertyValue("--speed"));
+
+let speed = 25;
+
+document.documentElement.style.setProperty("--speed", `${speed / 10}s`);
+
+let i = 0;
+
+startBtn.addEventListener("click", () => {
+    if (i == 0) {
+        i=1;
+    
+        let width = 0;
+        setInterval(frame, speed);
+        function frame() {
+            if(width >= 100) {
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+                elem.innerHTML.width = width + "%";
+                bgImg.classList.remove("blur");
+                bgImg.classList.add("clear");
+            }
+        }
+    }
+});
